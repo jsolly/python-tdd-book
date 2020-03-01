1a2d5232bbdd0f45577e40bb26307e0cc82549e9
John Solly <jsolly@protonmail.com>
2020-03-01 14:16:47 -0800

    Home page HTML now generated from a template!

--- functional_tests.py
+++ functional_tests.py
@@ -15,16 +15,18 @@ class NewVisitorTest(unittest.TestCase):
         # User wants to visit the home page of the todo list application
         self.browser.get('http://localhost:8000/')
 
-        # She notices the page header and title mention stuff about To-Do lists!
+        # She notices the page header and title mention To-Do lists!
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a todo item right away
-        inputbox = self.browser.find_element_by_tag_name('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a to-do item'
+        )
 
         # She types 'Buy peacock feathers' into the text box
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('1: Buy peacock feathers')
 
         # When she hits enter, the page updates to show:
         # "1: Buy Peacock Feathers" as an item in her todo list
@@ -35,7 +37,8 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
 
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do did not appear in table"
         )
 
         # She is now greeted with another empty text box in order to enter another item
