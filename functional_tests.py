93af8951d9ae266ff03c42f6f416edd44d9a2b94
John Solly <jsolly@protonmail.com>
2020-03-02 20:49:23 -0800

    Working on functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -26,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # She types 'Buy peacock feathers' into the text box
-        inputbox.send_keys('1: Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates to show:
         # "1: Buy Peacock Feathers" as an item in her todo list
@@ -36,19 +36,28 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
 
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # She is now greeted with another empty text box in order to enter another item
-        # She enters 'Use peacock feathers to make a lure.
-        self.fail('Finish the test!')
+        # She enters 'Use peacock feathers to make a fly.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # the page updates again, now showing two items.
-
-        #Edith wonders if the site will remember her lists 
-        #She notices that the site shows a unique url and some explanatory text
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
+        
+        # Edith wonders if the site will remember her lists 
+        # She notices that the site shows a unique url and some explanatory text
+        
+        self.fail('Finish the test!')
         # She visits that url and notices that her todo list was saved
         # Satisfied, she goes to sleep
 
