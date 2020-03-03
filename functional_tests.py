52e5135f1057e4af55cbc701e0d60f96ccdc6d3c
John Solly <jsolly@protonmail.com>
2020-03-02 21:01:57 -0800

    Refactored functional tests to include a helper func

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User wants to visit the home page of the todo list application
         self.browser.get('http://localhost:8000/')
@@ -32,11 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy Peacock Feathers" as an item in her todo list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # She is now greeted with another empty text box in order to enter another item
         # She enters 'Use peacock feathers to make a fly.
@@ -46,13 +47,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # the page updates again, now showing two items.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
         # Edith wonders if the site will remember her lists 
         # She notices that the site shows a unique url and some explanatory text
