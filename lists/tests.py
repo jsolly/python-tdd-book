716f18f49e008da2ec39f78cc19c15c9db2d94fd
John Solly <jsolly@protonmail.com>
2020-04-04 17:45:32 -0700

    Redirect after POST...and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from lists.views import home_page
 from lists.models import Item
+import unittest
 
 class HomePageTest(TestCase):
 
@@ -8,12 +9,30 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_can_save_a_POST_request(self): # This test is geetting a little long!
+        response = self.client.post('/', data={'item_text': "A new list item"})
+        
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={"item_text": "A new list item"})
-        self.assertIn("A new list item", response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': "A new list item"})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
 
+    def test_only_saves_items_when_needed(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemy 1")
+        Item.objects.create(text="itemy 2")
+        
+        response = self.client.get('/')
+
+        self.assertIn("itemy 1", response.content.decode())
+        self.assertIn("itemy 2", response.content.decode())
 
 class ItemModelTest(TestCase):
 
@@ -35,3 +54,6 @@ class ItemModelTest(TestCase):
 
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
